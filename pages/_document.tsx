28ce4fef2e8dea88efe341166a9225cf62d8c361
Stefan Tian <stefan55831@gmail.com>
2021-09-22 22:37:04 +0800

    ADD: Add ga tracking

---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable @next/next/next-script-for-ga */
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-Z29C8937YB"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Z29C8937YB', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
