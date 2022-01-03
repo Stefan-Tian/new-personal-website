dc76574c6506a8e09efe7f199fa1b106df5d353a
Sheng Hong Tian <stefan55831@gmail.com>
2022-01-03 22:16:27 +0800

    UPD: Update titles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Head from 'next/head';
 import ExperienceList from 'components/ExperienceList';
 import WordPiece from 'components/WordPiece';
 
@@ -6,29 +7,34 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className="home">
-      <div className="home__top-section">
-        <div className="home__intro">
-          Hi, I’m Stefan, a <WordPiece word="software engineer" /> specializing
-          in <WordPiece word="frontend" />.
-          <br />
-          <span className="home__intro--part2">
-            I have over 2 years of professional experience building production
-            ready web applications using <WordPiece word="React and Vue" />.
-          </span>
+    <>
+      <Head>
+        <title>Stefan Tien - Personal Website</title>
+      </Head>
+      <div className="home">
+        <div className="home__top-section">
+          <div className="home__intro">
+            Hi, I’m Stefan, a <WordPiece word="software engineer" />{' '}
+            specializing in <WordPiece word="frontend" />.
+            <br />
+            <span className="home__intro--part2">
+              I have over 2 years of professional experience building production
+              ready web applications using <WordPiece word="React and Vue" />.
+            </span>
+          </div>
+          <div className="home__image-wrapper">
+            <Image
+              width="100%"
+              height="100%"
+              src="https://ignyte-public.s3.us-west-1.amazonaws.com/personal-site-developer.svg"
+              layout="responsive"
+              alt="developer image"
+            />
+          </div>
         </div>
-        <div className="home__image-wrapper">
-          <Image
-            width="100%"
-            height="100%"
-            src="https://ignyte-public.s3.us-west-1.amazonaws.com/personal-site-developer.svg"
-            layout="responsive"
-            alt="developer image"
-          />
-        </div>
+        <ExperienceList />
       </div>
-      <ExperienceList />
-    </div>
+    </>
   );
 };
 
